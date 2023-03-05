5f599ef23bb4e9fa0c9124eddc674b70b3ae489d
Joseph Camacho <camacho.joseph@gmail.com>
2023-03-05 18:01:37 +0000

    Finished up the safety publisher (maybe?)

---
+++ src/safety/src/safety_publisher.py
@@ -0,0 +1,39 @@
+import rospy
+import math
+
+from ackermann_msgs.msg import AckermannDriveStamped
+
+from scan_subscriber import ScanListener
+
+class SafetyController:
+        DRIVE_TOPIC = "/vesc/high_level/ackermann_cmd_mux/output"
+        SAFETY_OVERWRITE_TOPIC = "/vesc/low_level/ackermann_cmd_mux/input/safety"
+        def __init__(self, run_on_callback=None):
+            """
+            run_on_callback: An optional function to run on callback in addition to our normal methods
+            """
+            self.drive_subscriber = rospy.Subscriber(self.DRIVE_TOPIC, AckermannDriveStamped, self.callback)
+            self.safety_publisher = rospy.Publisher(self.SAFETY_OVERWRITE_TOPIC, AckermannDriveStamped, queue_size=10)
+            self.scan_listener = ScanListener()
+
+            
+            self.drive_speed = 0.0
+            self.steering_angle = 0.0
+            self.run_on_callback = run_on_callback
+                                        
+        def callback(self, drive):
+            self.drive_speed = drive.drive.speed
+            self.steering_angle = drive.drive.steering_angle
+
+            angles, ranges = self.scan_listener.angles, self.scan_listener.ranges
+            danger_indexes = (angles < self.steering_angle + math.pi / 2) & (angles > self.steering_angle - math.pi / 2)
+            closest_distance = min(ranges[danger_indexes])
+
+            max_velocity = 0.1 * (closest_distance - 0.2)
+            if max_velocity >= self.drive_speed:
+                return
+            self.drive_speed = max_velocity
+            drive = AckermannDriveStamped()
+            drive.drive.drive_speed = self.drive_speed
+            drive.drive.steering_angle = self.steering_angle
+            self.safety_publisher.publish(drive)
--- src/safety/src/scan_subscriber.py
+++ src/safety/src/scan_subscriber.py
@@ -1,25 +1,17 @@
-#!/usr/bin/env python2
-
-import numpy as np
-
 import rospy
+
 from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import LaserScan
-from ackermann_msgs.msg import AckermannDriveStamped
 
-class ScanSubscriber:
-    SCAN_TOPIC = "/scan" # rospy.get_param("wall_follower/scan_topic")
-    def __init__(self):
-        self.subscriber = rospy.Subscriber(SCAN_TOPIC, numpy_msg(LaserScan), self.callback) 
-    
-    def callback(self, msg):
-        angle_min = msg.angle_min
-        angle_max = msg.angle_max
-        angle_increment = msg.angle_increment
-        ranges = msg.ranges
 
-if __name__ == "__main__":
-    rospy.init_node('scan_subscriber')
-    scan_subscriber = ScanSubscriber()
-    rospy.spin()
+class ScanListener:
+    SCAN_TOPIC = "/scan"
+    def __init__(self,):
+        self.scan_subscriber = rospy.Subscriber(self.SCAN_TOPIC, LaserScan, self.callback)
 
+        self.angles = []
+        self.ranges = []
+
+    def callback(self, lidar_scan):
+        self.angles = np.linspace(lidar_scan.angle_min, lidar_scan.angle_max, len(lidar_scan.ranges))x
+        self.ranges = np.array(lidar_scan.ranges)
