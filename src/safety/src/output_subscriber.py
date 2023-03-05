24a6c50ebf3c9cfaac777ac4137af89466f35a21
Joseph Camacho <camacho.joseph@gmail.com>
2023-03-05 17:27:14 +0000

    Wrote the code for `output_subscriber.py`

--- src/safety/src/output_subscriber.py
+++ src/safety/src/output_subscriber.py
@@ -1,6 +1,16 @@
 import rospy
 
+from ackermann_msgs.msg import AckermannDriveStamped
+
+
 class OutputListener:
+    OUTPUT_TOPIC = "/vesc/high_level/ackermann_cmd_mux/output"
     def __init__(self,):
-        pass
-    
+        self.output_subscriber = rospy.Subscriber(self.OUTPUT_TOPIC, AckermannDriveStamped, self.callback)
+
+        self.drive_speed = 0.0
+        self.steering_angle = 0.0
+
+    def callback(self, drive):
+        self.drive_speed = drive.drive.speed
+        self.steering_angle = drive.drive.steering_angle
