c650e1d3f426103564fbd013318472a902fde7e7
Joseph Camacho <camacho.joseph@gmail.com>
2023-03-08 01:04:09 +0000

    Changes to safety controller (mostly debugging)

--- src/safety/src/scan_subscriber.py
+++ src/safety/src/scan_subscriber.py
@@ -9,15 +9,22 @@ from sensor_msgs.msg import LaserScan
 
 class ScanListener:
     SCAN_TOPIC = "/scan"
-    def __init__(self,):
+    def __init__(self, run_on_callback=None):
+        """
+        run_on_callback: An optional function to run on callback.
+        """
+
         self.scan_subscriber = rospy.Subscriber(self.SCAN_TOPIC, LaserScan, self.callback)
 
         self.angles = []
         self.ranges = []
+	self.run_on_callback = run_on_callback
 
     def callback(self, lidar_scan):
         self.angles = np.linspace(lidar_scan.angle_min, lidar_scan.angle_max, len(lidar_scan.ranges))
-        self.ranges = np.array(lidar_scan.ranges)    
+        self.ranges = np.array(lidar_scan.ranges)
+	if self.run_on_callback is not None:
+            self.run_on_callback()
         
 if __name__ == '__main__':
     try:
